dd32ee7c87cbc6886256381a9a55d14f79f0112c
Samuel Henry Kurniawan <kurniawans@sea.com>
2022-04-07 23:10:37 +0800

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,16 @@ import { Link } from 'react-router-dom';
 import { Menu } from 'antd';
 
 function Navbar() {
-  return <></>;
+  return (
+    <Menu mode="horizontal" theme="dark">
+      <Menu.Item>
+        <Link to="">Home</Link>
+      </Menu.Item>
+      <Menu.Item>
+        <Link to="/games">Games</Link>
+      </Menu.Item>
+    </Menu>
+  );
 }
 
 export default Navbar;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,20 +1,36 @@
 import React from 'react';
 
 import { Switch, Route, Redirect } from 'react-router-dom';
+import styled from 'styled-components';
+
+import { Layout } from 'antd';
 
 import Auth from 'components/Auth';
+import Navbar from 'components/Navbar';
 
 import GamesPage from './GamesPage';
 import WelcomePage from './WelcomePage';
 
+const StyledHeader = styled(Layout.Header)`
+  height: 48px;
+  line-height: 48px;
+`;
+
 function HomePage() {
   return (
     <Auth>
-      <Switch>
-        <Route path="/games" component={GamesPage} />
-        <Route path="" component={WelcomePage} />
-        <Redirect to="" />
-      </Switch>
+      <Layout>
+        <StyledHeader>
+          <Navbar />
+        </StyledHeader>
+        <Layout>
+          <Switch>
+            <Route path="/games" component={GamesPage} />
+            <Route path="" component={WelcomePage} />
+            <Redirect to="" />
+          </Switch>
+        </Layout>
+      </Layout>
     </Auth>
   );
 }
