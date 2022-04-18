376beecc026846ef93bd818a2c9d32acda4da20f
Samuel Henry Kurniawan <kurniawans@sea.com>
2022-04-18 16:31:45 +0800

    background image

--- src/pages/HomePage/GamePage/GamePage.tsx
+++ src/pages/HomePage/GamePage/GamePage.tsx
@@ -5,6 +5,8 @@ import styled from 'styled-components';
 
 import { Layout } from 'antd';
 
+import Background from 'images/Background.jpg';
+
 import GameSidebar from './GameSidebar';
 import GuessPage from './GuessPage';
 import InfoPage from './InfoPage';
@@ -24,6 +26,29 @@ const GameContent = styled(Layout.Content)`
   min-height: 100%;
 `;
 
+const BackgroundImage = styled.div`
+  width: 100%;
+  height: 100%;
+  background-image: url(${Background});
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position-y: top;
+  background-position-x: 150px;
+  background-size: cover;
+`;
+
+const BackgroundColor = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: rgba(240, 242, 245, 0.3);
+`;
+
+const BackgroundPaper = styled.div`
+  width: 100%;
+  height: 100%;
+  background: rgba(240, 242, 245, 0.9);
+`;
+
 function GamePage() {
   return (
     <>
@@ -31,15 +56,21 @@ function GamePage() {
         <GameSidebar />
       </GameSider>
       <GameContent>
-        <Switch>
-          <Route path="/games/:gameId/leaderboard" component={LeaderboardPage} />
-          <Route path="/games/:gameId/info" component={InfoPage} />
-          <Route path="/games/:gameId/teams" component={TeamsPage} />
-          <Route path="/games/:gameId/phrases" component={PhrasesPage} />
-          <Route path="/games/:gameId/rounds/:roundId/guess" component={GuessPage} />
-          <Route path="/games/:gameId/rounds" component={RoundsPage} />
-          <Redirect to="/games/:gameId/info" />
-        </Switch>
+        <BackgroundImage>
+          <BackgroundColor>
+            <BackgroundPaper>
+              <Switch>
+                <Route path="/games/:gameId/leaderboard" component={LeaderboardPage} />
+                <Route path="/games/:gameId/info" component={InfoPage} />
+                <Route path="/games/:gameId/teams" component={TeamsPage} />
+                <Route path="/games/:gameId/phrases" component={PhrasesPage} />
+                <Route path="/games/:gameId/rounds/:roundId/guess" component={GuessPage} />
+                <Route path="/games/:gameId/rounds" component={RoundsPage} />
+                <Redirect to="/games/:gameId/info" />
+              </Switch>
+            </BackgroundPaper>
+          </BackgroundColor>
+        </BackgroundImage>
       </GameContent>
     </>
   );
